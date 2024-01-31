164636d5a2bdd9c2beecdd2577d6133eac532fd8
Heng Ma <maheng1991@hotmail.com>
2024-01-31 15:01:24 -0800

    add cosine dist to emb umap

--- examples/arxiv/build_emb_hf.py
+++ examples/arxiv/build_emb_hf.py
@@ -23,22 +23,23 @@ for i, row in tqdm(df.iterrows(), total=len(df), desc="Embedding Prog:"):
 
 emb_df = pd.DataFrame(emb_df)
 
+umap_emb = run_umap(emb_df.embedding.to_list(), n_components=2, metric="euclidean")
+emb_df["umapE_0"] = umap_emb[:, 0]
+emb_df["umapE_1"] = umap_emb[:, 1]
 
-umap_emb = run_umap(emb_df.embedding.to_list())
-
-emb_df["umap_0"] = umap_emb[:, 0]
-emb_df["umap_1"] = umap_emb[:, 1]
+umap_emb = run_umap(emb_df.embedding.to_list(), n_components=2, metric="cosine")
+emb_df["umapC_0"] = umap_emb[:, 0]
+emb_df["umapC_1"] = umap_emb[:, 1]
 
 emb_df.to_pickle("arxiv_hf.pkl")
 
-
-fig = px.scatter(
-    df.iloc[::],
-    x="umap_0",
-    y="umap_1",
-    color="cat",
-    hover_data=["id", "title", "categories"],
-    render_mode="webgl",
-)
-fig.update_traces(marker=dict(size=4))
-fig.write_html("umap_hf.html")
+# fig = px.scatter(
+#     df.iloc[::],
+#     x="umap_0",
+#     y="umap_1",
+#     color="cat",
+#     hover_data=["id", "title", "categories"],
+#     render_mode="webgl",
+# )
+# fig.update_traces(marker=dict(size=4))
+# fig.write_html("umap_hf.html")
